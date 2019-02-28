68caa93b1aa4fb79c3f563ad312cd4363d6ffed5
Jonny Fluckey <jonathon.fluckey@gmail.com>
2019-02-27 19:07:16 -0700

    changed controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  root 'subs#index'
+  
+  resources :subs do 
+    resources :topics
+  end
+  # get '/subs', to: 'sub#index'
+  # get '/subs/:id', to: 'subs#show'
+  # get '/subs/:id', to: 'subs#edit'
+  # put '/subs/:id', to: 'subs#update'
+  # delete '/subs/:id', to: 'subs#destroy'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
